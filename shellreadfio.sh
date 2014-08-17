2f29688ba1ccb56ef23e4f00229be1ee5e92b435
daokhactuan48 <khactuank55@yahoo.com>
2014-08-17 02:37:44 -0700

    Update shellreadfio.sh

--- shellreadfio.sh
+++ shellreadfio.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-apt-get install fio
-mkdir /root/fio
-mkdir /root/fio/read
 cd /root/fio/read
 wget https://raw.githubusercontent.com/daokhactuan48/Test/master/Read.fio
 fio Read.fio >> /root/fio/read/`date +"%d%m%y-%H%M%S"`-kq.txt
