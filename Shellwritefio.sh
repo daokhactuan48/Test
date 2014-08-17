65df17cae8ce751824fe6460462c5b93fbc4cbc0
daokhactuan48 <khactuank55@yahoo.com>
2014-08-17 02:31:14 -0700

    Create Shellwritefio.sh

---
+++ Shellwritefio.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get install fio
+mkdir /root/write
+cd /root/fio/write
+wget https://raw.githubusercontent.com/daokhactuan48/Test/master/Write.fio
+fio Read.fio >> /root/fio/write/`date +"%d%m%y-%H%M%S"`-kq.txt
